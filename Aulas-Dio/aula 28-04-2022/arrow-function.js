3ced141737ffb5e491c0a5188196d5f4ebdb3c47
Thiago Moutinho <thiagomoutinho34@gmail.com>
2022-05-02 16:28:00 -0300

    calculadora script

---
+++ Aulas-Dio/aula 28-04-2022/arrow-function.js
@@ -0,0 +1,6 @@
+const multiplicar = (x, y = 1) => {
+    console.log(x * y)
+}
+
+multiplicar(3, 2)
+multiplicar(3)
---
+++ Aulas-Dio/aula 28-04-2022/teste.js
@@ -0,0 +1,16 @@
+/* var nome = "Thiago";
+console.log(nome);
+
+var nome2 = "João";
+console.log(nome2); */
+
+let nome = "Thiago";
+let idade = 32;
+console.log(nome);
+
+function escrevendoNome() {
+    /* Variável de escopo bloco */
+    let nome = "Peres";
+    console.log(nome);
+    console.log(idade)
+}
